feb6c487a2d36a434e159b6c2bc71bb4ec5eae12
Rafael Guilber <20958795+CodifyBrazil@users.noreply.github.com>
2023-06-07 13:10:34 -0300

    :tada: feat: initail projects

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,11 @@
+// import '../../app/global.css'
+export default function Product() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Produtos</h1>
+      <button className="bg-sky-600 font-bold text-gray-300">
+        Ver produto
+      </button>
+    </div>
+  )
+}
